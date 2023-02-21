0bdc2ede29fee859ae50917e6d6421830516b4d9
gichuri <m.ndungu@alustudent.com>
2023-02-21 18:00:14 +0300

    add priviledges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,6 @@
+--create a new database and user
+--grant user priviledges
+
 CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db';
 CREATE USER IF NOT EXISTS 'hbnb_dev' IDENTIFIED BY  'hbnb_dev_pwd';
 GRANT ALL *.* TO 'hbnb_dev'@'localhost';
