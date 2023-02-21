7d8d8da890a7c596aecbafbfb222a21a39156eea
gichuri <m.ndungu@alustudent.com>
2023-02-21 17:58:37 +0300

    add priviledges

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db';
+CREATE USER IF NOT EXISTS 'hbnb_dev' IDENTIFIED BY  'hbnb_dev_pwd';
+GRANT ALL *.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema* TO 'hbnb_dev'@'localhost';
