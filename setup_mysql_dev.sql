b59cd863ea0881ee3260468e5519ec03f07cf5ab
gichuri <m.ndungu@alustudent.com>
2023-02-21 18:06:15 +0300

    correct db

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,6 @@
 --create a new database and user
 --grant user priviledges
-
-CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db';
-CREATE USER IF NOT EXISTS 'hbnb_dev' IDENTIFIED BY  'hbnb_dev_pwd';
-GRANT ALL *.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema* TO 'hbnb_dev'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
