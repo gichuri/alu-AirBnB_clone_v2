7a1c50a88ceef358ede8a768fde59f9380e7be8d
martin ndungu <M.ndungu@alustudent.com>
2023-03-10 13:44:57 +0300

    setup servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Install Nginx if it not already installed
+
+sudo apt-get update 
+sudo apt-get install nginx 
+
+# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+#Create a fake HTML file to test nginx
+echo "<html><head></head><body>Hello, World!</body></html>" | sudo tee /data/web_static/releases/test/index.html
+
+# create a symbolic link between 2 folders 
+
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+# give ownership to ubuntu user and group 
+
+sudo chown -R ubuntu:ubuntu /data/
+
+# serve content hbnb_static using alias
+sed -ri "55i location /hbnb_static/ {\nalias /data/web_static/current/;\n}\n" /etc/nginx/sites-available/default
+# restart nginx 
+sed -ri "55i location /hbnb_static/ {\nalias /data/web_static/current/;\n}\n" /etc/nginx/sites-available/default
+
