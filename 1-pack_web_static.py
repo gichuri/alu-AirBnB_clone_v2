2ed40d78a0a83542c84008c0343002ee23c46feb
martin ndungu <M.ndungu@alustudent.com>
2023-03-10 15:39:13 +0300

    add doc

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,13 @@
+#!/usr/bin/python3
+"""
+ module
+"""
 from fabric.api import local
 
 def do_pack():
+    """
+    generates a .tgz archive from the contents of the web_static folder
+    """
     local("mkdir -p versions")
     tgz_file = "versions/web_static_$(date '+%Y%m%d%H%M%S').tgz"
     try:
