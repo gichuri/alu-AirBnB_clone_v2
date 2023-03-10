f857e60fa8dc83e6142d2eac80594ef715b50302
gichuri <m.ndungu@alustudent.com>
2023-03-10 14:40:52 +0300

    add sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,14 +14,15 @@ sudo mkdir -p /data/web_static/shared/
 #Create a fake HTML file to test nginx
 echo "<html><head></head><body>Hello, World!</body></html>" | sudo tee /data/web_static/releases/test/index.html
 
-# create a symbolic link between 2 folders
-# if symbolic link exists,it should be deleted first
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# remove symbolic link between 2 folders 
+sudo rm /data/web_static/current
+# create a fsymbolic link between 2 folders 
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 # give ownership to ubuntu user and group 
 
 sudo chown -R ubuntu:ubuntu /data/
 
 # serve content hbnb_static using alias
-sed -ri "55i location /hbnb_static/ {\nalias /data/web_static/current/;\n}\n" /etc/nginx/sites-available/default
+sudo sed -ri "55i location /hbnb_static/ {\nalias /data/web_static/current/;\n}\n" /etc/nginx/sites-available/default
 # restart nginx 
 sudo service nginx restart
