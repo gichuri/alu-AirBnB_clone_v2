bb56e385f0ef95b388b347d7fcbe2c924ba6ca0d
martin ndungu <M.ndungu@alustudent.com>
2023-03-20 16:47:23 +0300

    flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@pp.route('/', strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+# start application on port 5000
+if __name__ == "__main__":
+    app.run(debug-True, host='0.0.0.0', port=5000)
