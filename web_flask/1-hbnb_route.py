901f51323aa18d788d3b4e257cb5134a9e8066bc
martin ndungu <M.ndungu@alustudent.com>
2023-03-20 18:03:24 +0300

    doc

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,52 +1,30 @@
 #!/usr/bin/python3
 """
-Flask Web Application
-
-This script creates a Flask web application that listens on 0.0.0.0, port 5000.
-It defines a single route for the homepage that displays the message "Hello HBNB!".
-
-Usage:
-    Run this script to start the Flask application:
-        python3 <filename>.py
-
+Write a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
 Routes:
-    /:
-        Displays the message "Hello HBNB!"
-
-Options:
-    - strict_slashes=False:
-        Ensures that trailing slashes are ignored in the route URL.
-
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+You must use the option strict_slashes=False in your route definition
 """
 
 from flask import Flask
-
-#create a new Flask web application
 app = Flask(__name__)
 
-#define a route for the homepage
-@app.route('/', strict_slashes=False)
-def home():
-    """
-    Displays a message on the homepage.
 
-    Returns:
-        A string with the message "Hello HBNB!".
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """display “Hello HBNB!”
     """
     return "Hello HBNB!"
-# define route for /hbnb
 
-@app.route('/hbnb' strict_slashes=False)
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    Defines a route for /hbnb
-    
-    Returns: 
-        A string with the message "HBNB"
+    """display “HBNB”
     """
     return "HBNB"
 
-# start the application on port 5000 and host 0.0.0.0
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
