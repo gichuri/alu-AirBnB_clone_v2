abe880f798fb0f3264b13d82e6b0ecf04664e1a4
gichuri <m.ndungu@alustudent.com>
2023-03-20 17:09:12 +0300

    doc

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-# create a new web application and configure ports
 
 from flask import Flask
+
+#create a new flask web application
+
 app = Flask(__name__)
 
+#define a route for the homepage
 @pp.route('/', strict_slashes=False)
 def home():
     return "Hello HBNB!"
 
-# start application on port 5000
+# start application on port 5000 and port 0.0.0.0
 if __name__ == "__main__":
     app.run(debug-True, host='0.0.0.0', port=5000)
