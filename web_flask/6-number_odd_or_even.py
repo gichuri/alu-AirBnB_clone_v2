036de35b1e79076d0bf68d47ce8708919f351a41
martin ndungu <M.ndungu@alustudent.com>
2023-03-21 20:40:30 +0300

    doc new

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-'''
+"""
 display a HTML page only if n is an integer:
 H1 tag: “Number: n is even|odd” inside the tag BODY
-'''
+"""
 
 from flask import Flask, render_template
 app = Flask(__name__)
