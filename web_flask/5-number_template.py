ef3811ec0371b8f4458270e87981da3696e0a78e
martin ndungu <M.ndungu@alustudent.com>
2023-03-21 19:52:29 +0300

    templates

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 """
 Write a script that starts a Flask web application:
-Your web application must be listening on 0.0.0.0, port 5000
-Routes:
-/: display “Hello HBNB!”
-/hbnb: display “HBNB”
-/c/<text>: display “C ” followed by the value of the text variable (replace
-underscore _ symbols with a space )
-/python/(<text>): display “Python ”, followed by the value of the text
-variable (replace underscore _ symbols with a space )
-    * The default value of text is “is cool”
+- /number_template/<n>: display a HTML page only if n is an integer:
+H1 tag: “Number: n” inside the tag BODY
 You must use the option strict_slashes=False in your route definition
 """
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -46,15 +39,15 @@ def python(text="is cool"):
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """display n is a number, only if n is an integer"""
+    """display “n is a number” only if n is an integer"""
     return "{} is a number".format(n)
 
-@app.route('number_template/<int:n>', strict_slashes=False)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    if isinstance(n,int):
-        return render_template('number.html', n=n)
-    else:
-        return render_template('404.html'),404
+    """display a HTML page only if n is an integer"""
+    return render_template('5-number.html', number=n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
