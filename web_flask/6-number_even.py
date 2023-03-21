88af43d0a9fa260d47baf3adf7c45afa9ddd3044
martin ndungu <M.ndungu@alustudent.com>
2023-03-21 20:28:27 +0300

    change doc

--- web_flask/6-number_even.py
+++ web_flask/6-number_even.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""
+
+'''
 Write a script that starts a Flask web application:
 Your web application must be listening on 0.0.0.0, port 5000
 Routes:
@@ -7,7 +8,7 @@ Routes:
 - /number_odd_or_even/<n>: display a HTML page only if n is an integer:
 H1 tag: “Number: n is even|odd” inside the tag BODY
 You must use the option strict_slashes=False in your route definition
-"""
+'''
 
 from flask import Flask, render_template
 app = Flask(__name__)
