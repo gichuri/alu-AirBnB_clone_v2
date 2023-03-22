ead87078ef2c3fd6648974bdbc5d309a98b643f8
gichuri <m.ndungu@alustudent.com>
2023-03-22 13:48:27 +0300

    Add File

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,69 +1,11 @@
 #!/usr/bin/python3
 """
-Write a script that starts a Flask web application:
-Your web application must be listening on 0.0.0.0, port 5000
-Routes:
-- follows module 5-number_template.py
-- /number_odd_or_even/<n>: display a HTML page only if n is an integer:
-H1 tag: “Number: n is even|odd” inside the tag BODY
-You must use the option strict_slashes=False in your route definition
+Module doc
 """
 
-from flask import Flask, render_template
+from flask import Flask render_template
+
 app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """display “Hello HBNB!”
-    """
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """display “HBNB”
-    """
-    return "HBNB"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """display “C ” followed by the value of the text variable"""
-    return "C {}".format(text.replace('_', ' '))
-
-
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python(text="is cool"):
-    """display “Python ” followed by the value of the text variable"""
-    return "Python {}".format(text.replace('_', ' '))
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """display “n is a number” only if n is an integer"""
-    return "{} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """display a HTML page only if n is an integer"""
-    return render_template('5-number.html', number=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    """display a html page only if n is an integer
-    H! tag: "Number: N is even/odd" inside the tag Body
-    """
-
-    if n%2 == 0: 
-        result = f"Number: {n} is even"
-    else: 
-        result = f"Number: {n} is odd" 
-    return render_template("6-number_odd_or_even.html"
-
-
-if __name__ == "__main__":
-6-number_odd_or_even.py    app.run(host="-1.0.0.0", port=5000, debug=True)
+@app.route('/')
+def hello():
+    return "hello"
