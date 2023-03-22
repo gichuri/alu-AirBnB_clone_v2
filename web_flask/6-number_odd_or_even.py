9eaabe7c1e2ad32d5345a5ee3832a425a5e4100d
martin <m.ndungu@alustudent.com>
2023-03-22 12:24:32 +0000

    rem nonAscii

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -5,7 +5,7 @@ Your web application must be listening on 0.0.0.0, port 5000
 Routes:
 - follows module 5-number_template.py
 - /number_odd_or_even/<n>: display a HTML page only if n is an integer:
-H1 tag: “Number: n is even|odd” inside the tag BODY
+H1 tag: Number: n is even|odd inside the tag BODY
 You must use the option strict_slashes=False in your route definition
 """
 
@@ -15,34 +15,34 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """display “Hello HBNB!”
+    """display Hello HBNB!
     """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """display “HBNB”
+    """display HBNB
     """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """display “C ” followed by the value of the text variable"""
+    """display C  followed by the value of the text variable"""
     return "C {}".format(text.replace('_', ' '))
 
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
-    """display “Python ” followed by the value of the text variable"""
+    """display Python  followed by the value of the text variable"""
     return "Python {}".format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """display “n is a number” only if n is an integer"""
+    """display n is a number only if n is an integer"""
     return "{} is a number".format(n)
 
 
@@ -55,12 +55,12 @@ def number_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """display a HTML page only if n is an integer
-       H1 tag: “Number: n is even|odd” inside the tag BODY
+       H1 tag: Number: n is even|odd inside the tag BODY
     """
     if n%2 ==0:
-        result = f"Number: {n} is even"
+        result = "Number: {n} is even".format(n)
     else:
-        result = f"Number: {n} is odd"
+        result = "Number: {n} is odd".format(n)
     return render_template("6-number_odd_or_even.html", result=result)
 
 
