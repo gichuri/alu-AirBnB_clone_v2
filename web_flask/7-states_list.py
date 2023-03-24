7479a284173374d92c6432f7471b7ab2927f8a3e
martin ndungu <M.ndungu@alustudent.com>
2023-03-24 22:26:37 +0300

    list states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,19 +2,7 @@
 
 """
 Write a script that starts a Flask web application:
-
-Your web application must be listening on 0.0.0.0, port 5000
-You must use storage for fetching data from the storage engine (FileStorage or DBStorage) => from models import storage and storage.all(...)
-After each request you must remove the current SQLAlchemy Session:
-Declare a method to handle @app.teardown_appcontext
-Call in this method storage.close()
-Routes:
-/states_list: display a HTML page: (inside the tag BODY)
-H1 tag: “States”
-UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
-LI tag: description of one State: <state.id>: <B><state.name></B>
-Import this 7-dump to have some data
-You must use the option strict_slashes=False in your route definition
+lists states in app
 """
 
 from flask import Flask, render_template
