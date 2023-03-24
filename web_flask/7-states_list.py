4220715a8587d8ac3901c87639734b720fce2353
martin <m.ndungu@alustudent.com>
2023-03-24 19:35:08 +0000

    correct documentation error

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,10 +1,15 @@
-#!/usr/bin/python3
-
+#! usr/bin/python3
+""" script that starts a Flask web application: Your web application must
+be listening on 0.0.0.0,
+port 5000.
+You must use storage for fetching data from the storage engine (FileStorage or
+DBStorage) => from models
+import storage and storage.all(...)
+After each request you must remove the current SQLAlchemy Session:
+    - Declare a method to handle @app.teardown_appcontext
+    - Call in this method storage.close()
+strict_slashes=False in your route definition
 """
-write a script that starts a web flask application
-fetch states from model.storage and 
-renders them to a web_page
-""" 
 
 from flask import Flask, render_template
 from models import storage
@@ -12,15 +17,20 @@ from models.state import State
 
 app = Flask(__name__)
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with state names"""
-    return render_template('7-states_list.html', states=storage.all('state').values())
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities():
+    """display a HTML page”
+    """
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State').values())
+
 
 @app.teardown_appcontext
 def teardown(self):
-    """remove sqlalchemy session"""
+    """remove the current SQLAlchemy Session"""
     storage.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
