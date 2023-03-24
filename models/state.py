fb4bf751d218e41d60e0c8bdaece62ac86ced61e
martin ndungu <M.ndungu@alustudent.com>
2023-03-24 21:55:36 +0300

    improve engine

--- models/state.py
+++ models/state.py
@@ -5,4 +5,23 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
+
+    if (getenv("HBNB_TYPE_STORAGE") != "db"):
+        @property
+        def cities(self):
+            """
+            returns the list of City instances with state_id equals to the
+            current State.id
+            """
+            city_list = []
+            cities_list = models.storage.all(City).values()
+            for city in cities_list:
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
